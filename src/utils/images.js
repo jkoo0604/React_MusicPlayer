7fe6631ef68bf30f7ce94d37c231ced01f60340a
jkoo0604 <63644766+jkoo0604@users.noreply.github.com>
2021-01-10 11:53:26 -0800

    util for assets

---
+++ src/utils/images.js
@@ -0,0 +1,11 @@
+const images = {
+    img1: require('../assets/images/1.jpg').default,
+    img2: require('../assets/images/2.jpg').default,
+    img3: require('../assets/images/3.jpg').default,
+    img4: require('../assets/images/4.jpg').default,
+    img5: require('../assets/images/5.jpg').default,
+    img6: require('../assets/images/6.jpg').default,
+    img7: require('../assets/images/7.jpg').default,
+};
+
+export default images;
---
+++ src/utils/musicInfo.js
@@ -0,0 +1,34 @@
+const musicInfo = [
+    {
+        id: 1,
+        artist: 'Yomoti',
+        title: 'Before Chill',
+    },
+    {
+        id: 2,
+        artist: 'Matt Large',
+        title: 'Dusty Conscience',
+    },
+    {
+        id: 3,
+        artist: 'Mai Ligne',
+        title: 'Funky Street',
+    },
+    {
+        id: 4,
+        artist: 'SINY',
+        title: 'Hainan Paradise',
+    },
+    {
+        id: 5,
+        artist: 'SINY',
+        title: 'NEAR',
+    },
+    {
+        id: 6,
+        artist: 'Dylan Sitts',
+        title: 'Ten Days',
+    },
+];
+
+export default musicInfo;
