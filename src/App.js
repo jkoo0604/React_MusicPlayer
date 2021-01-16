4d3625c23690501aaaa94889716ccbe2047a29d7
jkoo0604 <63644766+jkoo0604@users.noreply.github.com>
2021-01-16 11:12:04 -0800

    firebase added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/analytics';
 
 import Player from './components/Player';
+import { firebaseConfig } from './config/firebaseConfig';
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+};
+firebase.analytics();
 
 function App() {
     return (
